2c53673a5aefeea66fa5a76326831f4521159858
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2022-01-04 15:02:06 +0100

    added some more on branscher and customer

--- components/section1.js
+++ components/section1.js
@@ -1,10 +1,16 @@
+import styles from '../styles/Section1.module.scss'
 import CustomerCard from "./customerCard";
 
 const Section1 = () => {
   return (
     <>
-      <h1>Våra kunder säger</h1>
+    <h1>Våra kunder tycker</h1>
+    <div className={styles.container}>
       <CustomerCard comment='"Jag tycker att techonomic tillför exakt det jag behöver i min bokföring."' name='Petter, mäklare' />
+      <div className={styles.hide}>
+      <CustomerCard className={styles.hide} comment='"Snabbt och effektivt, klart bästa redovisningsekonomer jag haft!"' name='Ola, utvecklare' />
+      </div>
+    </div>
     </>
   );
 };
