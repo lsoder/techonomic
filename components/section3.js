b0de64fa07c3aa0bdba0e222db3f52839040a96d
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2022-01-04 17:08:16 +0100

    fixat med mobil vs desktop

--- components/section3.js
+++ components/section3.js
@@ -1,14 +1,21 @@
+import Image from 'next/image';
+import styles from '../styles/Section1.module.scss';
+
 const Section3 = () => {
-    return (
-      <>
+  return (
+    <div className={styles.hideLarge}>
+      <div className={styles.container3}>
         <h1>Branscher</h1>
+        <Image src="/money.png" alt="logo" width="200" height="200" />
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor
+          Teknik, ekonomi och personliga relationer driver Techonomic framåt.
         </p>
-      </>
-    );
-  };
-  
-  export default Section3;
+        <h3>Mäklare</h3>
+        <h3>Redovisningsekonomer</h3>
+        <h3>Finansbolag</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Section3;
