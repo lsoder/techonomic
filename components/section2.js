d88872135f28164297d0ada83ce30c1ec9427dab
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2022-01-05 14:58:10 +0100

    add materialUI and change black adn purple buttons

--- components/section2.js
+++ components/section2.js
@@ -1,14 +1,13 @@
-import styles from '../styles/Section1.module.scss'
+import styles from '../styles/Section1.module.scss';
+import { BlackButton } from './buttons';
 
 const Section2 = () => {
   return (
     <div className={`${styles.container2} ${styles.hideLarge}`}>
       <div className={`${styles.bookMeetingContent}`}>
-      <h1>Boka ett möte med oss</h1>
-      <button>Till kalender</button>
-      <p>
-        Lorem ipsum dolor sit amet,
-      </p>
+        <h1>Boka ett möte med oss</h1>
+        <BlackButton text="till kalender" />
+        <p>Lorem ipsum dolor sit amet,</p>
       </div>
     </div>
   );
