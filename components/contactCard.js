fbfec072ca01defc6e0ed90e67e8cd0b6d7734ca
lsoder <lindaevasoder@gmail.com>
2022-01-07 15:38:04 +0100

    contact page and some icons

---
+++ components/contactCard.js
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import styles from '../styles/ContactCard.module.scss'
+import InstagramIcon from '@mui/icons-material/Instagram'
+import FacebookIcon from '@mui/icons-material/Facebook'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import { BlackButton } from './buttons'
+
+const ContactCard = (props) => {
+  return (
+    <div className={styles.container}>
+      <Image src={props.image} alt='logo' width='100' height='100' />
+      <p>{props.name}</p>
+      <h4>{props.mail}</h4>
+      <h4>{props.phone}</h4>
+      <div className={styles.some}>
+        <FacebookIcon fontSize='small' />
+        <LinkedInIcon fontSize='small' />
+        <InstagramIcon fontSize='small' />
+      </div>
+      <BlackButton text='Boka möte' />
+    </div>
+  )
+}
+
+export default ContactCard
--- pages/kontakta.js
+++ pages/kontakta.js
@@ -1,8 +1,34 @@
+import styles from '../styles/Contact.module.scss'
+import ContactCard from '../components/contactCard'
+
 const Kontakta = () => {
   return (
-    <>
-      <h2>Kontakta-sidan</h2>
-    </>
+    <div className={styles.container}>
+      <h2>Kontakta oss</h2>
+      <div className={styles.textContainer}>
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet!
+          <br />
+          <br />
+          Frågor? Prata med någon av våra experter!
+        </p>
+      </div>
+      <div className={styles.cardContainer}>
+        <ContactCard
+          image='/avatar-male.png'
+          name='Namn Efternamn'
+          mail='mail@mail.com'
+          phone='073 000000'
+        />
+        <ContactCard
+          image='/avatar-male.png'
+          name='Namn Efternamn'
+          mail='mail@mail.com'
+          phone='073 000000'
+        />
+      </div>
+    </div>
   )
 }
 
