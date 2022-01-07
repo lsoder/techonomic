186d554b63568c6c6ac52e19364a3d59fb579939
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2022-01-07 15:21:56 +0100

    add tile on different pages

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,17 @@
+import Head from 'next/head';
 const About = () => {
-  return <h2>Om Techonomic</h2>
-}
+  return (
+    <>
+      <Head>
+        <title>Techonomic - OM OSS</title>
+        <meta
+          name="description"
+          content="Meta description for the About page"
+        />
+      </Head>
+      <h2>Om Techonomic</h2>
+    </>
+  );
+};
 
-export default About
+export default About;
--- pages/kontakta.js
+++ pages/kontakta.js
@@ -1,9 +1,18 @@
+import Head from 'next/head';
+
 const Kontakta = () => {
   return (
     <>
+      <Head>
+        <title>Techonomic - KONTAKTA OSS</title>
+        <meta
+          name="description"
+          content="Meta description for the About page"
+        />
+      </Head>
       <h2>Kontakta-sidan</h2>
     </>
-  )
-}
+  );
+};
 
-export default Kontakta
+export default Kontakta;
