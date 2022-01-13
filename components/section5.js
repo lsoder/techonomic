8ed524fd59b29a6458790d80c0b2cb7e8e39fff3
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2022-01-13 18:40:56 +0100

    lite små-pill

--- components/section5.js
+++ components/section5.js
@@ -17,13 +17,27 @@ const Section5 = () => {
           underText="Detta ingår i vårt grundpaket, vi erhåller
         även andra tjänster att lägga till."
         >
-          <p>{bull} Personlig konsult</p>
-          <p>{bull} Rådgivning</p>
-          <p>{bull} Löpande bokföring</p>
-          <p>{bull} Kvartals-/års-moms</p>
-          <p>{bull} Arbetsgivardeklaration</p>
-          <p>{bull} Programvarukostnad</p>
-          <p>{bull} Grafisk rapporteringsverktyg</p>
+          <div className={styles.listItem}>
+            {bull} <p>Personlig konsult</p>
+          </div>
+          <div className={styles.listItem}>
+            {bull} <p>Rådgivning</p>
+          </div>
+          <div className={styles.listItem}>
+            {bull} <p>Löpande bokföring</p>
+          </div>
+          <div className={styles.listItem}>
+            {bull} <p>Kvartals-/års-moms</p>
+          </div>
+          <div className={styles.listItem}>
+            {bull} <p>Arbetsgivardeklaration</p>
+          </div>
+          <div className={styles.listItem}>
+            {bull} <p>Programvarukostnad</p>
+          </div>
+          <div className={styles.listItem}>
+            {bull} <p>Grafisk rapporteringsverktyg</p>
+          </div>
           <div className={styles.buttonDiv}>
             <ButtonSm href="/abonnemang" text="Läs mer" />
           </div>
@@ -34,11 +48,26 @@ const Section5 = () => {
           link="/abonnemang"
           underText="Dessa tillägg man kan lägga till i abonnemanget."
         >
-          <p>{plus} Betalning av lev.fakturor</p>
-          <p>{plus} Månadsmoms</p>
-          <p>{plus} Löner</p>
-          <p>{plus} Periodisk sammanställning</p>
-          <p>{plus} Årstjänster</p>
+          <div className={styles.listItem}>
+            {plus}
+            <p>Betalning av lev.fakturor</p>
+          </div>
+          <div className={styles.listItem}>
+            {plus}
+            <p>Månadsmoms</p>
+          </div>
+          <div className={styles.listItem}>
+            {plus}
+            <p>Löner</p>
+          </div>
+          <div className={styles.listItem}>
+            {plus}
+            <p>Periodisk sammanställning</p>
+          </div>
+          <div className={styles.listItem}>
+            {plus}
+            <p>Årstjänster</p>
+          </div>
         </SubscriptionCard>
       </div>
     </>
--- pages/branscher.js
+++ pages/branscher.js
@@ -2,6 +2,7 @@ import styles from '../styles/Branscher.module.scss';
 import CustomerCard from '../components/customerCard';
 import CustomerCardReverse from '../components/customerCardReverse';
 import Head from 'next/head';
+import { gear } from '../components/coloredIcons';
 
 const Branscher = () => {
   return (
@@ -14,28 +15,59 @@ const Branscher = () => {
         />
       </Head>
       <div className={styles.container}>
-        <div className={styles.customerDiv}>
-          <CustomerCard
-            image="/images/petter.png"
-            comment='Jag tycker att techonomic tillför exakt det jag behöver i min bokföring.'
-            name="Petter, mäklare"
-          />
-          <CustomerCardReverse
-            image="/images/ola.png"
-            comment='Snabbt och effektivt, dem har gjort underverk för mig och min firma.'
-            name="Nils, målare"
-          />
-        </div>
-        <div className={styles.branscherDiv}>
-          <h3>Några branscher vi specificerat oss på*</h3>
-          <p>mäklare</p>
-          <p>mäklare</p>
-          <p>mäklare</p>
-          <p>mäklare</p>
-          <i>
-            * tveka inte att höra av dig om du tillhör någon annan bransch, vi
-            tror på det bla bla lba
-          </i>
+        <h2>Branscher</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        </p>
+        <div className={styles.content}>
+          <div className={styles.customerDiv}>
+            <CustomerCard
+              image="/images/petter.png"
+              comment="Jag tycker att techonomic tillför exakt det jag behöver i min bokföring."
+              name="Petter, mäklare"
+            />
+            <CustomerCardReverse
+              image="/images/ola.png"
+              comment="Snabbt och effektivt, dem har gjort underverk för mig och min firma."
+              name="Nils, målare"
+            />
+          </div>
+          <div className={styles.branscherDiv}>
+            <h3>Några branscher vi specificerat oss på*</h3>
+            <div className={styles.branschList}>
+              <div className={styles.listItem}>
+                {gear} <p>Mäklare</p>
+              </div>
+              <div className={styles.listItem}>
+                {gear} <p>Utvecklare</p>
+              </div>
+              <div className={styles.listItem}>
+                {gear} <p>Influencers</p>
+              </div>
+              <div className={styles.listItem}>
+                {gear} <p>Tatuerare</p>
+              </div>
+              <div className={styles.listItem}>
+                {gear} <p>Snickare</p>
+              </div>
+              <div className={styles.listItem}>
+                {gear} <p>Frisöres</p>
+              </div>
+            </div>
+            <i>
+              * tveka inte att höra av dig om du tillhör någon annan bransch, vi
+              tror på det bla bla lba
+            </i>
+          </div>
         </div>
       </div>
     </>
