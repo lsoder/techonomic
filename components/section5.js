cc2d5442f9ed8f7c12e9c1db269012f2ca435d9f
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2022-01-14 14:32:04 +0100

    add passHref

--- components/section5.js
+++ components/section5.js
@@ -78,7 +78,7 @@ const Section5 = () => {
         <SubscriptionDesktop />
         <i>
           Vill du veta mer om vårt abonnemang och övriga tjänster klicka{' '}
-          <Link href="/abonnemang">
+          <Link href="/abonnemang" passHref>
             <b>här.</b>
           </Link>
         </i>
