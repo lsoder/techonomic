bbb6cad1fc0ff851ec7cb7028c09dfcf4150d331
lsoder <lindaevasoder@gmail.com>
2022-01-18 21:45:27 +0100

    added text

--- components/section1.js
+++ components/section1.js
@@ -1,24 +1,38 @@
-import styles from '../styles/Section1.module.scss';
+import styles from '../styles/Section1.module.scss'
 import SubscriptionCardBasic, {
   SubscriptionCardExtra,
-} from './subscriptionCards/subscriptionCards';
-import Lottie from "lottie-react";
+} from './subscriptionCards/subscriptionCards'
+import Lottie from 'lottie-react'
 // import animation from "../lottie/pattern.json"
-import FadeIn from '../motion/fadeIn';
+import FadeIn from '../motion/fadeIn'
 
 const Section1 = () => {
   return (
     <>
       {/* <Section5/> */}
-      <div className={styles.Subscription}>
-        <FadeIn>
-        <SubscriptionCardBasic />
-        </FadeIn>
-        {/* <Lottie className={styles.animation} animationData={animation}></Lottie> */}
-        {/* <SubscriptionCardExtra/> */}
+      <div className={styles.container}>
+        {' '}
+        <div className={styles.textContainer}>
+          <h2>Techonomic på 5 sek.</h2>
+          <p>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident.
+            <br />
+            <br />
+            Välkommen till oss!
+          </p>
+        </div>
+        <div className={styles.Subscription}>
+          <FadeIn>
+            <SubscriptionCardBasic />
+          </FadeIn>
+          {/* <Lottie className={styles.animation} animationData={animation}></Lottie> */}
+          {/* <SubscriptionCardExtra/> */}
+        </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Section1;
+export default Section1
