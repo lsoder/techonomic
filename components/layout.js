1e4bf1a5180aea868a9cc9b83e814f7712d2ded2
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2022-01-18 14:49:36 +0100

    fix new mouse

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,15 @@
 import Navbar from './nav/nav'
 import Footer from './footer/footer'
+import Box from '../components/cursor';
 
 export default function Layout({ children }) {
   return (
     <>
+    <Box>
       <Navbar />
       <main>{children}</main>
       <Footer />
+    </Box>
     </>
   )
 }
