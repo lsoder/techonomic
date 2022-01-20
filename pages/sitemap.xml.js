efff6d6e13a7adfef1485ac3d6b2166e2bcf086a
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2022-01-20 16:34:39 +0100

    another try

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,36 +1,34 @@
+
 //pages/sitemap.xml.js
-const BASE_URL = 'https://fervent-payne-fceded.netlify.app/';
+const EXTERNAL_DATA_URL = 'https://fervent-payne-fceded.netlify.app/'
 
 function generateSiteMap(posts) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <!--We manually set the two URLs we know already-->
      <url>
-	 <loc>${`${BASE_URL}`}</loc>
+       <loc>https://fervent-payne-fceded.netlify.app/abonnemang</loc>
      </url>
      <url>
-	 <loc>${`${BASE_URL}abonnemang`}</loc>
+       <loc>https://fervent-payne-fceded.netlify.app/branscher</loc>
      </url>
      <url>
-	 <loc>${`${BASE_URL}branscher`}</loc>
+       <loc>https://fervent-payne-fceded.netlify.app/about</loc>
      </url>
      <url>
-	 <loc>${`${BASE_URL}about`}</loc>
-     </url>
-     <url>
-	 <loc>${`${BASE_URL}kontakta`}</loc>
+       <loc>https://fervent-payne-fceded.netlify.app/kontakta</loc>
      </url>
      ${posts
        .map(({ id }) => {
          return `
        <url>
-           <loc>${`${BASE_URL}/${id}`}</loc>
+           <loc>${`${EXTERNAL_DATA_URL}/${id}`}</loc>
        </url>
-     `;
+     `
        })
        .join('')}
    </urlset>
- `;
+ `
 }
 
 function SiteMap() {
@@ -39,20 +37,36 @@ function SiteMap() {
 
 export async function getServerSideProps({ res }) {
   // We make an API call to gather the URLs for our site
-  const request = await fetch(BASE_URL);
-  const posts = await request.json();
+  const request = await fetch(EXTERNAL_DATA_URL)
+  const posts = await request.json()
 
   // We generate the XML sitemap with the posts data
-  const sitemap = generateSiteMap(posts);
+  const sitemap = generateSiteMap(posts)
 
-  res.setHeader('Content-Type', 'text/xml');
+  res.setHeader('Content-Type', 'text/xml')
   // we send the XML to the browser
-  res.write(sitemap);
-  res.end();
+  res.write(sitemap)
+  res.end()
 
   return {
-    props: {},
-  };
+    props: {}
+  }
 }
 
-export default SiteMap;
+export default SiteMap
+
+{/* <url>
+<loc>${`${BASE_URL}`}</loc>
+</url>
+<url>
+<loc>${`${BASE_URL}abonnemang`}</loc>
+</url>
+<url>
+<loc>${`${BASE_URL}branscher`}</loc>
+</url>
+<url>
+<loc>${`${BASE_URL}about`}</loc>
+</url>
+<url>
+<loc>${`${BASE_URL}kontakta`}</loc>
+</url> */}
