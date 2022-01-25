d1d5c4bf4a1967cf6cde9e41f7e04227869e0b12
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2022-01-25 15:50:11 +0100

    section2 fix

--- pages/index.js
+++ pages/index.js
@@ -21,8 +21,8 @@ export default function Home({ content }) {
         <Hero />
         <section1 id='tech'>{content}</section1>
         <Section1 />
-        {/* <Section2 />
-        <Section3 />
+        <Section2 />
+        {/* <Section3 />
         <Section4 />
         <Section5 />  */}
       </>
