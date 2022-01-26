2b745c0caff2cede4043691082b0dd83f026e4b5
lsoder <lindaevasoder@gmail.com>
2022-01-26 13:57:42 +0100

    fix scroll to

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { style } from '@mui/system'
 import Head from 'next/head'
 import Hero from '../components/hero/hero'
 import Section1 from '../components/section1/section1'
@@ -6,6 +7,7 @@ import Section3 from '../components/section3/section3'
 import Section4 from '../components/section4/section4'
 import Section5 from '../components/section5/section5'
 // import Section6 from '../components/section5/section6'
+import styles from '../styles/Home.module.scss'
 
 export default function Home({ content }) {
   return (
@@ -19,7 +21,9 @@ export default function Home({ content }) {
       </Head>
       <>
         <Hero />
-        <section1 id='tech'>{content}</section1>
+        <div id='tech' className={styles.tech}>
+          {content}
+        </div>
         <Section1 />
         <Section2 />
         <Section3 />
