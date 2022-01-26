f56d3695b026a07a83605427d866c552ca297f9d
lsoder <lindaevasoder@gmail.com>
2022-01-26 21:49:37 +0100

    add new styling

--- pages/branscher.js
+++ pages/branscher.js
@@ -4,6 +4,11 @@ import CustomerCard, {
 } from '../components/customerCards/customerCards'
 import Head from 'next/head'
 import { gear } from '../components/coloredIcons'
+import BranschCard from '../components/branschCard/branschCard'
+
+import Some from '../components/some'
+import Image from 'next/image'
+import FadeIn from '../motion/fadeIn'
 
 const Branscher = () => {
   return (
@@ -16,7 +21,7 @@ const Branscher = () => {
           content='Meta description for the bransch page'
         />
       </Head>
-      <div className={styles.container}>
+      {/*   <div className={styles.container}>
         <h2>Branscher</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -53,6 +58,46 @@ const Branscher = () => {
               * tveka inte att höra av dig om du tillhör någon annan bransch, vi
               kan säkert hitta en lösning för er också!
             </i>
+          </div>
+        </div>
+      </div> */}
+
+      {/* ny kod */}
+
+      <div className={styles.branschContainer}>
+        <div className={styles.textContainer}>
+          <FadeIn>
+            <h2>Branscher som vi jobbar med</h2>
+          </FadeIn>
+          <FadeIn>
+            <p>
+              Teknik, ekonomi och personliga relationer driver Techonomic
+              framåt. Vi är övertygade om att kombinationen av detta är rätt väg
+              att gå - kryddat med mycket glädje. Vi har skapat ett
+              redovisningsabonnemang där vi blir din digitala ekonomiavdelning.
+              Det ska vara enkelt, personligt och skräddarsytt för dig och din
+              verksamhet.
+            </p>
+          </FadeIn>
+          <FadeIn>
+            <i> Vill du veta mer? Prata med någon av våra experter!</i>
+          </FadeIn>
+        </div>
+        <div className={styles.bransch}>
+          <div className={styles.branscherDiv}>
+            <CustomerCard
+              image='/images/petter.png'
+              comment='Jag tycker att techonomic tillför exakt det jag behöver i min bokföring.'
+              name='Petter, mäklare'
+            />
+            <CustomerCardReverse
+              image='/images/ola.png'
+              comment='Snabbt och effektivt, dem har gjort underverk för mig och min firma.'
+              name='Nils, målare'
+            />
+          </div>
+          <div className={styles.branschCard}>
+            <BranschCard />
           </div>
         </div>
       </div>
