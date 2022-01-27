7bd45cb724bf3da94827a2e9e694bd1ff638cac4
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2022-01-27 14:19:35 +0100

    fix i abonnemang

--- pages/abonnemang.js
+++ pages/abonnemang.js
@@ -13,8 +13,8 @@ const Abonnemang = () => {
         <title>Techonomic - Abonnemang</title>
 
         <meta
-          name='description'
-          content='Vi levererar en heldigitaliserad redovisningslösning till en fast kostnad. Inga extra kostnader för uppstart eller andra dolda avgifter. Utan bindningstid och uppsägningstid - flexibelt och enkelt. '
+          name="description"
+          content="Vi levererar en heldigitaliserad redovisningslösning till en fast kostnad. Inga extra kostnader för uppstart eller andra dolda avgifter. Utan bindningstid och uppsägningstid - flexibelt och enkelt. "
         />
       </Head>
       <div className={styles.subscriptionWrapperDesktop}>
@@ -49,50 +49,52 @@ const Abonnemang = () => {
           <SubscriptionDesktop />
         </FadeIn>
         <FadeIn>
-        <div className={styles.content}>
-          <div className={styles.box2}>
-            <h2>
-              <span>Plustjänster</span>
-            </h2>
-            <div>
-              <div className={styles.listItem}>
-                {filledPlus}
-                <p>Betalning av lev.fakturor</p>
-              </div>
-              <div className={styles.listItem}>
-                {filledPlus}
-                <p>Månadsmoms</p>
-              </div>
-              <div className={styles.listItem}>
-                {filledPlus}
-                <p>Löner</p>
-              </div>
-              <div className={styles.listItem}>
-                {filledPlus}
-                <p>Periodisk sammanställning</p>
-              </div>
-              <div className={styles.listItem}>
-                {filledPlus}
-                <p>Årstjänster</p>
+          <div className={styles.content}>
+            <div className={styles.box2}>
+              <h2>
+                <span>Plustjänster</span>
+              </h2>
+              <div>
+                <div className={styles.listItem}>
+                  {filledPlus}
+                  <p>Betalning av lev.fakturor</p>
+                </div>
+                <div className={styles.listItem}>
+                  {filledPlus}
+                  <p>Månadsmoms</p>
+                </div>
+                <div className={styles.listItem}>
+                  {filledPlus}
+                  <p>Löner</p>
+                </div>
+                <div className={styles.listItem}>
+                  {filledPlus}
+                  <p>Periodisk sammanställning</p>
+                </div>
+                <div className={styles.listItem}>
+                  {filledPlus}
+                  <p>Årstjänster</p>
+                </div>
+                <i>
+                  Vi skräddarsyr efter dina behov genom att addera t.ex.
+                  betalningar, integrationer, periodisk sammanställning etc.
+                </i>
               </div>
             </div>
+            <div className={styles.box3}>
+              <h2>Vad är detta?</h2>
+              <p>
+                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat. Duis aute irure dolor in reprehenderit in
+                voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                qui officia deserunt mollit anim id est laborum.
+              </p>
+              <i>Hör av er för att få ett personligt prisförslag.</i>
+            </div>
           </div>
-          <div className={styles.box3}>
-            <h2>Vad är detta?</h2>
-            <p>
-              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-              labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-              nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-              consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-              occaecat cupidatat non proident, sunt in culpa qui officia
-              deserunt mollit anim id est laborum.
-            </p>
-            <i>
-              Hör av er för att få ett personligt prisförslag.
-            </i>
-          </div>
-        </div>
         </FadeIn>
       </div>
     </>
