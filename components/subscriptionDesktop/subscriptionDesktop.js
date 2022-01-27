fae2cb2ff361bd755d62685c86c56a8b29309b0d
AmandaStahlberg <stahlberg.amanda@hotmail.com>
2022-01-27 23:05:41 +0100

    liten liten fix

--- components/subscriptionDesktop/subscriptionDesktop.js
+++ components/subscriptionDesktop/subscriptionDesktop.js
@@ -13,7 +13,7 @@ import { ButtonSm } from '../buttons';
 const SubscriptionDesktop = () => {
   return (
     <div className={styles.containerSubscriptionDesktop}>
-       <h2>Abonnemang</h2>
+       <h2>I abonnemanget ingår</h2>
       <div className={styles.gridContainer}>
         <div className={styles.gridContent}>
           <LoyaltyIcon fontSize="large" />
